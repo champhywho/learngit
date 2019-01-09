9fc9d90fb799bdcee1c432ba466db8338dcc1026
champhywho <champhywho@gmail.com>
2019-01-09 17:05:27 +0800

    change content

--- hello.py
+++ hello.py
@@ -1,12 +1,1 @@
-def createCounter():
-    ans=[0]
-    def counter():
-        ans[0]+=1
-        print(ans)
-        return ans[0]
-    print(ans)
-    return counter
-A=createCounter()
-print(A())
-print(A())
-print(A())
+print('hello world!')
